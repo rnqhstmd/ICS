a8871be23d63b2b5b8729c8f0782b987d517257c
BonSeung <bs.koo@sqisoft.com>
2024-09-10 17:25:45 +0900

    setting: Spring Initializing

---
+++ src/main/java/org/example/sqi_images/SqiImagesApplication.java
@@ -0,0 +1,13 @@
+package org.example.sqi_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqiImagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqiImagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sqi_images/SqiImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqi_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqiImagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
