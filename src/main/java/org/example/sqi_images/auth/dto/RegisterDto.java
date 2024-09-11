f244f99a00bf31cd1aa9e86f4c09200875072549
BonSeung <bs.koo@sqisoft.com>
2024-09-11 17:39:18 +0900

    feat: dto 클래스 타입 record로 변경 - #7

--- src/main/java/org/example/sqi_images/auth/dto/RegisterDto.java
+++ src/main/java/org/example/sqi_images/auth/dto/RegisterDto.java
@@ -6,16 +6,17 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
-public class RegisterDto {
-    @NotBlank(message = "사내 이메일을 입력해주세요.")
-    @Pattern(regexp = "^[a-z]+\\.[a-z]+$", message = "이메일 형식이 올바르지 않습니다.")
-    private String email;
+public record RegisterDto(
+        @NotBlank(message = "사내 이메일을 입력해주세요.")
+        @Pattern(regexp = "^[a-z]+\\.[a-z]+$", message = "이메일 형식이 올바르지 않습니다.")
+        String email,
 
-    @NotBlank(message = "영문과 숫자,특수기호를 조합하여 8 ~ 20글자로 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()])[A-Za-z\\d!@#$%^&*()]{8,20}$", message = "비밀번호는 영문과 숫자,특수기호를 조합하여 8~20글자 미만으로 입력해주세요.")
-    private String password;
+        @NotBlank(message = "영문과 숫자,특수기호를 조합하여 8 ~ 20글자로 입력해주세요.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()])[A-Za-z\\d!@#$%^&*()]{8,20}$", message = "비밀번호는 영문과 숫자,특수기호를 조합하여 8~20글자 미만으로 입력해주세요.")
+        String password,
 
-    @NotBlank(message = "사용하실 이름을 입력해주세요.")
-    @Size(min = 1, max = 10, message = "이름은 최소 1자에서 최대 10자까지 입력 가능합니다.")
-    private String name;
+        @NotBlank(message = "사용하실 이름을 입력해주세요.")
+        @Size(min = 1, max = 10, message = "이름은 최소 1자에서 최대 10자까지 입력 가능합니다.")
+        String name
+) {
 }
