3fd375d6f355f330336e3cd8e16d9f16e7f488ec
BonSeung <bs.koo@sqisoft.com>
2024-09-11 15:53:14 +0900

    feat: ConflictException 생성 - #5

---
+++ src/main/java/org/example/sqi_images/common/exception/ConflictException.java
@@ -0,0 +1,15 @@
+package org.example.sqi_images.common.exception;
+
+
+import org.example.sqi_images.common.exception.type.ErrorType;
+import org.springframework.http.HttpStatus;
+
+public class ConflictException extends ApiException {
+    public ConflictException(final ErrorType errorType) {
+        super(errorType, HttpStatus.CONFLICT);
+    }
+
+    public ConflictException(final ErrorType errorType, final String detail) {
+        super(errorType, detail, HttpStatus.CONFLICT);
+    }
+}
