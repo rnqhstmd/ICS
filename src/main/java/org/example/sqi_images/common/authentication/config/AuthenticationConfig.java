2f4b102a677cf7241e21a67f4536f874b86e71c3
BonSeung <bs.koo@sqisoft.com>
2024-09-11 17:26:56 +0900

    feat: 권한 관련 config 생성 - #7

---
+++ src/main/java/org/example/sqi_images/common/authentication/config/AuthenticationConfig.java
@@ -0,0 +1,34 @@
+package org.example.sqi_images.common.authentication.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.sqi_images.common.authentication.AuthenticatedEmployeeArgumentResolver;
+import org.example.sqi_images.common.authentication.AuthenticationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final AuthenticatedEmployeeArgumentResolver authenticatedEmployeeArgumentResolver;
+    private static final String ADD_AUTH_API_PATH = "/api/auth/logout";
+    private static final String[] EXCLUDE_AUTH_API_PATH = {"/api/auth/login", "/api/auth/sign-up"};
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns(ADD_AUTH_API_PATH)
+                .excludePathPatterns(EXCLUDE_AUTH_API_PATH);
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticatedEmployeeArgumentResolver);
+    }
+}
+
