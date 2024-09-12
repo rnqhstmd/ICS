42006b78f553ed87697bf6c1c5c2a3839202bad5
BonSeung <bs.koo@sqisoft.com>
2024-09-12 13:33:24 +0900

    feat: 프로필 전체 조 추가 - #13

--- src/main/java/org/example/sqi_images/profile/service/ProfileService.java
+++ src/main/java/org/example/sqi_images/profile/service/ProfileService.java
@@ -5,11 +5,15 @@ import org.example.sqi_images.common.exception.ForbiddenException;
 import org.example.sqi_images.employee.domain.Employee;
 import org.example.sqi_images.profile.domain.Profile;
 import org.example.sqi_images.profile.dto.request.CreateProfileDto;
+import org.example.sqi_images.profile.dto.response.ProfileResponse;
+import org.example.sqi_images.profile.dto.response.ProfileResponseList;
 import org.example.sqi_images.profile.repository.ProfileRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.example.sqi_images.common.exception.type.ErrorType.PROFILE_ALREADY_EXISTS_ERROR;
 
@@ -37,5 +41,16 @@ public class ProfileService {
                 employee
         );
         profileRepository.save(profile);
+    }
+
+    @Transactional(readOnly = true)
+    public ProfileResponseList getAllProfiles() {
+        List<ProfileResponse> profiles = profileRepository.findAll().stream()
+                .map(profile -> ProfileResponse.of(
+                        profile.getId(),
+                        profile.getEmployee().getName(),
+                        profile.getPhoto()))
+                .toList();
+        return ProfileResponseList.from(profiles);
     }
 }
