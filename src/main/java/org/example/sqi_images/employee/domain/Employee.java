7662c49e78f1a02d79710c6c1c52d7baa36f4b36
BonSeung <bs.koo@sqisoft.com>
2024-09-13 13:31:52 +0900

    feat: File 일대다 관계 추가 - #17

--- src/main/java/org/example/sqi_images/employee/domain/Employee.java
+++ src/main/java/org/example/sqi_images/employee/domain/Employee.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.sqi_images.common.domain.BaseEntity;
+import org.example.sqi_images.file.domain.File;
 import org.example.sqi_images.profile.domain.Profile;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,8 +26,11 @@ public class Employee extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-    @OneToOne(mappedBy = "employee", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
     private Profile profile;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<File> files;
 
     public Employee(String email, String password, String name) {
         this.email = email;
