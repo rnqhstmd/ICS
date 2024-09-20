6b8f0ebc27633ce23221997d69f018da19008373
BonSeung <bs.koo@sqisoft.com>
2024-09-20 13:52:49 +0900

    feat: 업로드된 파일 전체 조회 API 추가 - #25

--- src/main/java/org/example/sqi_images/file/controller/FileController.java
+++ src/main/java/org/example/sqi_images/file/controller/FileController.java
@@ -3,17 +3,20 @@ package org.example.sqi_images.file.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.sqi_images.auth.authentication.annotation.Authenticated;
 import org.example.sqi_images.employee.domain.Employee;
+import org.example.sqi_images.file.domain.File;
 import org.example.sqi_images.file.dto.request.FileInfoUploadDto;
+import org.example.sqi_images.common.dto.page.request.PageRequestDto;
 import org.example.sqi_images.file.dto.response.FileDownloadDto;
+import org.example.sqi_images.file.dto.response.FileListDto;
+import org.example.sqi_images.common.dto.page.response.PageResultDto;
 import org.example.sqi_images.file.service.FileService;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
+import static org.example.sqi_images.file.util.FileUtil.createFileDownloadHeaders;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,20 +30,22 @@ public class FileController {
                                              @RequestPart("file") MultipartFile file,
                                              @RequestPart("fileName") FileInfoUploadDto fileInfoUploadDto) throws IOException {
         fileService.uploadFile(employee, fileInfoUploadDto, file);
-        return ResponseEntity.status(HttpStatus.CREATED).body("프로필 생성 완료");
+        return ResponseEntity.status(HttpStatus.CREATED).body("파일 업로드 완료");
     }
 
     @GetMapping("/download/{fileId}")
     public ResponseEntity<byte[]> downloadFile(@PathVariable Long fileId) {
         FileDownloadDto fileDownloadDto = fileService.downloadFile(fileId);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentDispositionFormData("attachment", fileDownloadDto.fileName());
-        headers.setContentType(MediaType.parseMediaType(fileDownloadDto.contentType()));
-        headers.setContentLength(fileDownloadDto.fileSize());
-
+        HttpHeaders headers = createFileDownloadHeaders(fileDownloadDto);
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(fileDownloadDto.data());
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<PageResultDto<FileListDto, File>> listFiles(@RequestParam(defaultValue = "1") int page) {
+        PageRequestDto pageRequestDto = new PageRequestDto(page);
+        PageResultDto<FileListDto, File> fileList = fileService.listFiles(pageRequestDto);
+        return ResponseEntity.ok(fileList);
     }
 }
