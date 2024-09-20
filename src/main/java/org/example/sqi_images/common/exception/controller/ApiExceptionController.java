545e6bde4e936a525a4bfb5c31be5e1e2d96337b
BonSeung <bs.koo@sqisoft.com>
2024-09-20 13:49:23 +0900

    feat: Multipart 관련 예외 핸들러 추가 - #25

--- src/main/java/org/example/sqi_images/common/exception/controller/ApiExceptionController.java
+++ src/main/java/org/example/sqi_images/common/exception/controller/ApiExceptionController.java
@@ -8,10 +8,13 @@ import org.example.sqi_images.common.exception.type.ErrorType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
@@ -53,6 +56,37 @@ public class ApiExceptionController {
                 ErrorType.METHOD_NOT_ALLOWED_ERROR, httpRequestMethodNotSupportedException.getMessage());
 
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(ApiExceptionResponse.res(badRequestException));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ApiExceptionResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException exception) {
+        String detail = "업로드 가능한 파일의 크기는 5MB 미만입니다.";
+        ApiExceptionResponse response = new ApiExceptionResponse(
+                ErrorType.UPLOAD_FILE_SIZE_LIMIT_EXCEEDED.getErrorCode(),
+                ErrorType.UPLOAD_FILE_SIZE_LIMIT_EXCEEDED.getMessage(),
+                detail
+        );
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(response);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<ApiExceptionResponse> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e) {
+        BadRequestException badRequestException = new BadRequestException(
+                ErrorType.MEDIA_TYPE_NOT_ACCEPTABLE_ERROR,
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(ApiExceptionResponse.res(badRequestException));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ApiExceptionResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        BadRequestException badRequestException = new BadRequestException(
+                ErrorType.MEDIA_TYPE_NOT_SUPPORTED_ERROR,
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
                 .body(ApiExceptionResponse.res(badRequestException));
     }
 
