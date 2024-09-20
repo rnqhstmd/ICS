d3e4d2788d9788c616de6ca377d616c18adaf594
BonSeung <bs.koo@sqisoft.com>
2024-09-20 13:53:17 +0900

    feat: 업로드된 파일 전체 조회 로직 추가 - #25

--- src/main/java/org/example/sqi_images/file/service/FileService.java
+++ src/main/java/org/example/sqi_images/file/service/FileService.java
@@ -1,26 +1,30 @@
 package org.example.sqi_images.file.service;
-
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.apache.tika.Tika;
 import org.example.sqi_images.common.exception.NotFoundException;
 import org.example.sqi_images.common.exception.type.ErrorType;
 import org.example.sqi_images.file.domain.File;
 import org.example.sqi_images.file.dto.request.FileInfoUploadDto;
+import org.example.sqi_images.common.dto.page.request.PageRequestDto;
 import org.example.sqi_images.file.dto.response.FileDownloadDto;
+import org.example.sqi_images.file.dto.response.FileListDto;
+import org.example.sqi_images.common.dto.page.response.PageResultDto;
 import org.example.sqi_images.file.repository.FileRepository;
 import org.example.sqi_images.employee.domain.Employee;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import static org.example.sqi_images.file.util.FileUtil.*;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
     private final FileRepository fileRepository;
-    private final Tika tika = new Tika();
 
     /**
      * 파일 업로드
@@ -30,10 +34,14 @@ public class FileService {
         String fileName = generateUniqueFileName(uploadDTO.fileName());
         byte[] fileData = file.getBytes();
         long fileSize = file.getSize();
-        String contentType = tika.detect(fileData);
+        String formattedFileSize = formatFileSize(fileSize);
 
-        File newFile = new File(fileName, fileData, contentType, fileSize, employee);
-        fileRepository.save(newFile);
+        try (InputStream input = file.getInputStream()) {
+            String contentType = tika.detect(input);
+            String fileExtension = getFileExtensionByMimeType(input);
+            File newFile = new File(fileName, fileData, contentType, fileExtension, fileSize, formattedFileSize, employee);
+            fileRepository.save(newFile);
+        }
     }
 
     /**
@@ -45,9 +53,9 @@ public class FileService {
 
         return FileDownloadDto.of(
                 file.getFileName(),
-                file.getContentType(),
+                file.getFileContentType(),
                 file.getFileSize(),
-                file.getFile()
+                file.getFileData()
         );
     }
 
@@ -64,5 +72,14 @@ public class FileService {
             fileName = originalName + " (" + count + ")";
         }
         return fileName;
+    }
+
+    /**
+     * 업로드된 파일 전체 조회
+     */
+    @Transactional(readOnly = true)
+    public PageResultDto<FileListDto, File> listFiles(PageRequestDto pageRequestDto) {
+        Page<File> result = fileRepository.findAll(pageRequestDto.toPageable());
+        return new PageResultDto<>(result, FileListDto::of);
     }
 }
