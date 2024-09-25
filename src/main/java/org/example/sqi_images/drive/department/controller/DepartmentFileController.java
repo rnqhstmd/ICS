f383c543afa9a2d7bc67daceee22c7a04f3f0529
BonSeung <bs.koo@sqisoft.com>
2024-09-25 10:01:18 +0900

    feat: 파일 업로드 방식 변경 - #35

--- src/main/java/org/example/sqi_images/drive/department/controller/DepartmentFileController.java
+++ src/main/java/org/example/sqi_images/drive/department/controller/DepartmentFileController.java
@@ -1,18 +1,19 @@
 package org.example.sqi_images.drive.department.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.sqi_images.auth.authentication.annotation.AuthEmployee;
 import org.example.sqi_images.common.aop.annotation.DepartmentMember;
 import org.example.sqi_images.common.dto.page.request.PageRequestDto;
 import org.example.sqi_images.common.dto.page.response.PageResultDto;
-import org.example.sqi_images.drive.common.dto.request.FileInfoUploadDto;
 import org.example.sqi_images.drive.common.dto.response.FileDownloadDto;
 import org.example.sqi_images.drive.common.dto.response.FileListDto;
 import org.example.sqi_images.drive.department.domain.DepartmentFile;
 import org.example.sqi_images.drive.department.service.DepartmentFileService;
 import org.example.sqi_images.employee.domain.Employee;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,20 +33,22 @@ public class DepartmentFileController {
     @DepartmentMember
     public ResponseEntity<String> uploadDepartmentFile(@AuthEmployee Employee employee,
                                                        @PathVariable Long departmentId,
-                                                       @RequestPart("file") MultipartFile file,
-                                                       @RequestPart("fileName") @Valid FileInfoUploadDto fileInfoUploadDto) throws IOException {
-        departmentFileService.uploadDepartmentFile(employee, departmentId, fileInfoUploadDto, file);
+                                                       @RequestPart("file") MultipartFile file) throws IOException {
+        departmentFileService.uploadDepartmentFile(employee, departmentId, file);
         return ResponseEntity.status(HttpStatus.CREATED).body("부서별 공유 드라이브 파일 업로드 완료");
     }
 
     @GetMapping("/download/{fileId}")
     @DepartmentMember
-    public ResponseEntity<byte[]> downloadDepartmentFile(@PathVariable Long departmentId,
-                                                         @PathVariable Long fileId) {
+    public ResponseEntity<ByteArrayResource> downloadDepartmentFile(@PathVariable Long departmentId,
+                                                                    @PathVariable Long fileId) {
         FileDownloadDto fileDownloadDto = departmentFileService.downloadDepartmentFile(fileId);
+        HttpHeaders headers = createFileDownloadHeaders(fileDownloadDto);
+
         return ResponseEntity.ok()
-                .headers(createFileDownloadHeaders(fileDownloadDto))
-                .body(fileDownloadDto.data());
+                .contentType(MediaType.valueOf(fileDownloadDto.contentType()))
+                .headers(headers)
+                .body(new ByteArrayResource(fileDownloadDto.data()));
     }
 
     @GetMapping("/list")
