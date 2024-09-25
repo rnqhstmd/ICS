386844662c41d75a2afd973f8d4914d3b1a20cb6
BonSeung <bs.koo@sqisoft.com>
2024-09-25 10:00:59 +0900

    feat: 업로드 파일 이름 읿력 Dto 삭제 - #35

--- src/main/java/org/example/sqi_images/drive/common/dto/request/FileInfoUploadDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.sqi_images.drive.common.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record FileInfoUploadDto(
-        @NotBlank(message = "파일 이름을 작성해주세요.")
-        String fileName
-) {
-}
