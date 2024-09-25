1a014a29db8e415177ba1b52fe3eae9109b54b98
BonSeung <bs.koo@sqisoft.com>
2024-09-25 10:01:15 +0900

    feat: 파일 업로드 방식 변경 - #35

--- src/main/java/org/example/sqi_images/drive/department/service/DepartmentFileService.java
+++ src/main/java/org/example/sqi_images/drive/department/service/DepartmentFileService.java
@@ -8,7 +8,6 @@ import org.example.sqi_images.common.exception.NotFoundException;
 import org.example.sqi_images.common.exception.type.ErrorType;
 import org.example.sqi_images.department.domain.Department;
 import org.example.sqi_images.department.domain.repository.DepartmentRepository;
-import org.example.sqi_images.drive.common.dto.request.FileInfoUploadDto;
 import org.example.sqi_images.drive.common.dto.response.FileDownloadDto;
 import org.example.sqi_images.drive.common.dto.response.FileListDto;
 import org.example.sqi_images.drive.department.domain.DepartmentFile;
@@ -20,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import static org.example.sqi_images.common.exception.type.ErrorType.DEPARTMENT_NOT_FOUND_ERROR;
 import static org.example.sqi_images.common.exception.type.ErrorType.UPLOADED_FILE_EMPTY_ERROR;
@@ -34,24 +32,27 @@ public class DepartmentFileService {
     private final DepartmentRepository departmentRepository;
 
     @Transactional
-    public void uploadDepartmentFile(Employee employee, Long departmentId, FileInfoUploadDto fileInfoUploadDto, MultipartFile file) throws IOException {
+    public void uploadDepartmentFile(Employee employee, Long departmentId, MultipartFile file) throws IOException {
         if (file.isEmpty()) {
             throw new BadRequestException(UPLOADED_FILE_EMPTY_ERROR);
         }
+        // 파일 데이터 추출
         byte[] fileData = file.getBytes();
         long fileSize = file.getSize();
-        String fileName = generateUniqueFileName(fileInfoUploadDto.fileName());
+
+        // 파일 이름, 확장자 추출
+        String fileName = generateUniqueFileName(file.getOriginalFilename());
+        String fileExtension = getExtensionByFileName(fileName);
+
         String formattedFileSize = formatFileSize(fileSize);
         Department department = departmentRepository.findById(departmentId).orElseThrow(() -> new NotFoundException(DEPARTMENT_NOT_FOUND_ERROR));
 
-        try (InputStream input = file.getInputStream()) {
-            String contentType = tika.detect(input);
-            String fileExtension = getFileExtensionByMimeType(input);
-            DepartmentFile newFile = new DepartmentFile(fileName, fileData, contentType, fileExtension, fileSize, formattedFileSize, employee, department);
-            departmentFileRepository.save(newFile);
-        }
+        String contentType = file.getContentType();
+
+        DepartmentFile newFile = new DepartmentFile(fileName, fileData, contentType, fileExtension, fileSize, formattedFileSize, employee, department);
+        departmentFileRepository.save(newFile);
     }
-    
+
     public FileDownloadDto downloadDepartmentFile(Long fileId) {
         DepartmentFile file = departmentFileRepository.findById(fileId)
                 .orElseThrow(() -> new NotFoundException(ErrorType.FILE_NOT_FOUND_ERROR));
