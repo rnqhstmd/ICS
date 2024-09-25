c0402afccf1f255fb2999f28f826b96d8a85c58f
BonSeung <bs.koo@sqisoft.com>
2024-09-25 15:10:03 +0900

    feat: 지연 로딩으로 인한 N+1문제 쿼리 작성으로 해결 - #35

--- src/main/java/org/example/sqi_images/drive/department/domain/repository/DepartmentFileRepository.java
+++ src/main/java/org/example/sqi_images/drive/department/domain/repository/DepartmentFileRepository.java
@@ -4,8 +4,14 @@ import org.example.sqi_images.drive.department.domain.DepartmentFile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DepartmentFileRepository extends JpaRepository<DepartmentFile, Long> {
-    Page<DepartmentFile> findByDepartmentId(Long departmentId, Pageable pageable);
     boolean existsByFileName(String originalName);
+    @Query("SELECT df FROM DepartmentFile df " +
+            "JOIN FETCH df.employee e " +
+            "JOIN FETCH e.department " +
+            "JOIN FETCH e.part " +
+            "WHERE df.department.id = :departmentId ")
+    Page<DepartmentFile> findByDepartmentIdWithEmployee(Long departmentId, Pageable pageable);
 }
