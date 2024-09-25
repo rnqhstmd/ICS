bb08ce5544d1ecbf7f769e5f443103b6b1f5ba5a
BonSeung <bs.koo@sqisoft.com>
2024-09-25 10:02:42 +0900

    feat: 파일 업로드 방식 변경 - #35

--- src/main/java/org/example/sqi_images/drive/global/controller/GlobalFileController.java
+++ src/main/java/org/example/sqi_images/drive/global/controller/GlobalFileController.java
@@ -1,17 +1,18 @@
 package org.example.sqi_images.drive.global.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.sqi_images.auth.authentication.annotation.AuthEmployee;
 import org.example.sqi_images.common.dto.page.request.PageRequestDto;
 import org.example.sqi_images.common.dto.page.response.PageResultDto;
-import org.example.sqi_images.drive.common.dto.request.FileInfoUploadDto;
 import org.example.sqi_images.drive.common.dto.response.FileDownloadDto;
 import org.example.sqi_images.drive.common.dto.response.FileListDto;
 import org.example.sqi_images.drive.global.domain.GlobalFile;
 import org.example.sqi_images.drive.global.service.GlobalFileService;
 import org.example.sqi_images.employee.domain.Employee;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,18 +30,20 @@ public class GlobalFileController {
 
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
     public ResponseEntity<String> uploadFile(@AuthEmployee Employee employee,
-                                             @RequestPart("file") MultipartFile file,
-                                             @RequestPart("fileName") @Valid FileInfoUploadDto fileInfoUploadDto) throws IOException {
-        globalFileService.uploadGlobalFile(employee, fileInfoUploadDto, file);
+                                             @RequestPart("file") MultipartFile file) throws IOException {
+        globalFileService.uploadGlobalFile(employee, file);
         return ResponseEntity.status(HttpStatus.CREATED).body("전체 공유 드라이브 파일 업로드 완료");
     }
 
     @GetMapping("/download/{fileId}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable Long fileId) {
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Long fileId) {
         FileDownloadDto fileDownloadDto = globalFileService.downloadGlobalFile(fileId);
+        HttpHeaders headers = createFileDownloadHeaders(fileDownloadDto);
+
         return ResponseEntity.ok()
-                .headers(createFileDownloadHeaders(fileDownloadDto))
-                .body(fileDownloadDto.data());
+                .contentType(MediaType.valueOf(fileDownloadDto.contentType()))
+                .headers(headers)
+                .body(new ByteArrayResource(fileDownloadDto.data()));
     }
 
     @GetMapping("/list")
--- src/main/java/org/example/sqi_images/drive/global/service/GlobalFileService.java
+++ src/main/java/org/example/sqi_images/drive/global/service/GlobalFileService.java
@@ -8,7 +8,6 @@ import org.example.sqi_images.common.exception.NotFoundException;
 import org.example.sqi_images.common.exception.type.ErrorType;
 import org.example.sqi_images.department.domain.Department;
 import org.example.sqi_images.department.domain.repository.DepartmentRepository;
-import org.example.sqi_images.drive.common.dto.request.FileInfoUploadDto;
 import org.example.sqi_images.drive.common.dto.response.FileDownloadDto;
 import org.example.sqi_images.drive.common.dto.response.FileListDto;
 import org.example.sqi_images.drive.global.domain.GlobalFile;
@@ -20,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import static org.example.sqi_images.common.exception.type.ErrorType.DEPARTMENT_NOT_FOUND_ERROR;
 import static org.example.sqi_images.common.exception.type.ErrorType.UPLOADED_FILE_EMPTY_ERROR;
@@ -34,25 +32,24 @@ public class GlobalFileService {
     private final DepartmentRepository departmentRepository;
 
     @Transactional
-    public void uploadGlobalFile(Employee employee, FileInfoUploadDto fileInfoUploadDto, MultipartFile file) throws IOException {
-        Department department = departmentRepository.findById(employee.getDepartment().getId())
-                .orElseThrow(() -> new NotFoundException(DEPARTMENT_NOT_FOUND_ERROR));
-
+    public void uploadGlobalFile(Employee employee, MultipartFile file) throws IOException {
         if (file.isEmpty()) {
             throw new BadRequestException(UPLOADED_FILE_EMPTY_ERROR);
         }
 
         byte[] fileData = file.getBytes();
         long fileSize = file.getSize();
-        String fileName = generateUniqueFileName(fileInfoUploadDto.fileName());
-        String formattedFileSize = formatFileSize(fileSize);
 
-        try (InputStream input = file.getInputStream()) {
-            String contentType = tika.detect(input);
-            String fileExtension = getFileExtensionByMimeType(input);
-            GlobalFile newFile = new GlobalFile(fileName, fileData, contentType, fileExtension, fileSize, formattedFileSize, employee, department);
-            globalFileRepository.save(newFile);
-        }
+        String fileName = generateUniqueFileName(file.getOriginalFilename());
+        String fileExtension = getExtensionByFileName(fileName);
+        Department department = departmentRepository.findById(employee.getDepartment().getId())
+                .orElseThrow(() -> new NotFoundException(DEPARTMENT_NOT_FOUND_ERROR));
+
+        String formattedFileSize = formatFileSize(fileSize);
+        String contentType = file.getContentType();
+
+        GlobalFile newFile = new GlobalFile(fileName, fileData, contentType, fileExtension, fileSize, formattedFileSize, employee, department);
+        globalFileRepository.save(newFile);
     }
 
     public FileDownloadDto downloadGlobalFile(Long fileId) {
