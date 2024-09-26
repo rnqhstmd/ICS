d06f8d951bea7cf4edef077a1b4e6225e9f57888
BonSeung <bs.koo@sqisoft.com>
2024-09-26 14:06:21 +0900

    feat: 프로필 조회 시 employeeId 반환하게끔 수정 - #41

--- src/main/java/org/example/sqi_images/employee/dto/response/ProfileDetailResponse.java
+++ src/main/java/org/example/sqi_images/employee/dto/response/ProfileDetailResponse.java
@@ -6,6 +6,7 @@ import org.example.sqi_images.common.domain.LanguageType;
 import org.example.sqi_images.common.domain.PartType;
 
 public record ProfileDetailResponse(
+        Long employeeId,
         String name,
         String email,
         DepartmentType department,
@@ -14,13 +15,15 @@ public record ProfileDetailResponse(
         FrameworkType frameworkType,
         String photoUrl
 ) {
-    public static ProfileDetailResponse of(String name,
-                                           String email,
-                                           DepartmentType department,
-                                           PartType partType,
-                                           LanguageType languageType,
-                                           FrameworkType frameworkType,
-                                           String photoUrl) {
-        return new ProfileDetailResponse(name, email, department, partType, languageType, frameworkType, photoUrl);
+    public static ProfileDetailResponse of(
+            Long employeeId,
+            String name,
+            String email,
+            DepartmentType department,
+            PartType partType,
+            LanguageType languageType,
+            FrameworkType frameworkType,
+            String photoUrl) {
+        return new ProfileDetailResponse(employeeId, name, email, department, partType, languageType, frameworkType, photoUrl);
     }
 }
