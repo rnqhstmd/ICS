c8dfe48fe3159da8a00d7e967ae3ad7f4962e7c9
BonSeung <bs.koo@sqisoft.com>
2024-09-27 15:37:31 +0900

    feat: 전체 조회 시 사용될 Dto 추가 - #45

---
+++ src/main/java/org/example/sqi_images/file/dto/response/FileInfoResponseDto.java
@@ -0,0 +1,27 @@
+package org.example.sqi_images.file.dto.response;
+
+import org.example.sqi_images.employee.domain.Employee;
+import org.example.sqi_images.file.domain.FileInfo;
+
+import static org.example.sqi_images.common.constant.Constants.FORMATTER;
+
+public record FileInfoResponseDto(
+        Long fileId,
+        String fileName,
+        String uploaderName,
+        String uploadedDate,
+        String fileExtension,
+        String formattedSize
+) {
+    public static FileInfoResponseDto from(FileInfo file) {
+        Employee employee = file.getEmployee();
+        return new FileInfoResponseDto(
+                file.getId(),
+                file.getFileName(),
+                employee.getName(),
+                file.getCreatedAt().format(FORMATTER),
+                file.getFileExtension(),
+                file.getFormattedFileSize()
+        );
+    }
+}
