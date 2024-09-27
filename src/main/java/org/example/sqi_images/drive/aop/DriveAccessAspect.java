cb3e271141bda1f7f5c709a8cc515db874f5d626
BonSeung <bs.koo@sqisoft.com>
2024-09-27 15:29:40 +0900

    feat: AOP 를 이용한 커스텀 어노테이션 구현 코드 작성 - #45

---
+++ src/main/java/org/example/sqi_images/drive/aop/DriveAccessAspect.java
@@ -0,0 +1,52 @@
+package org.example.sqi_images.drive.aop;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.example.sqi_images.auth.authentication.AuthenticationContext;
+import org.example.sqi_images.common.exception.ForbiddenException;
+import org.example.sqi_images.drive.aop.annotation.CheckDriveAccess;
+import org.example.sqi_images.drive.domain.DriveEmployee;
+import org.example.sqi_images.drive.domain.repository.DriveEmployeeRepository;
+import org.example.sqi_images.employee.domain.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+import static org.example.sqi_images.common.exception.type.ErrorType.NO_ADMIN_ACCESS_ERROR;
+import static org.example.sqi_images.common.exception.type.ErrorType.NO_DRIVE_ACCESS_ERROR;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class DriveAccessAspect {
+
+    private final AuthenticationContext authenticationContext;
+    private final DriveEmployeeRepository driveEmployeeRepository;
+
+    @Pointcut("@annotation(checkDriveAccess)")
+    public void driveAccessCheckPointcut(CheckDriveAccess checkDriveAccess) {}
+
+    @Around("driveAccessCheckPointcut(checkDriveAccess)")
+    public Object verifyDriveAccess(ProceedingJoinPoint joinPoint, CheckDriveAccess checkDriveAccess) throws Throwable {
+        // 현재 인증된 Employee 객체 가져오기
+        Employee employee = authenticationContext.getPrincipal();
+        Long employeeId = employee.getId();
+
+        // 메서드 파라미터에서 driveId 추출
+        Object[] args = joinPoint.getArgs();
+        Long driveId = (Long) args[0];
+
+        // 권한 체크 로직 수행
+        DriveEmployee driveEmployee = driveEmployeeRepository.findByDriveIdAndEmployeeId(driveId, employeeId)
+                .orElseThrow(() -> new ForbiddenException(NO_DRIVE_ACCESS_ERROR));
+
+        if (Arrays.stream(checkDriveAccess.accessType())
+                .noneMatch(requiredRole -> driveEmployee.getRole() == requiredRole)) {
+            throw new ForbiddenException(NO_ADMIN_ACCESS_ERROR);
+        }
+        return joinPoint.proceed();
+    }
+}
