8508c55edc0344b5de84933991b4765b8bd955f6
BonSeung <bs.koo@sqisoft.com>
2024-09-27 15:32:10 +0900

    feat: DriveEmployee 매핑 추 - #45

--- src/main/java/org/example/sqi_images/employee/domain/Employee.java
+++ src/main/java/org/example/sqi_images/employee/domain/Employee.java
@@ -5,8 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.sqi_images.common.domain.*;
-import org.example.sqi_images.drive.department.domain.DepartmentFile;
-import org.example.sqi_images.drive.global.domain.GlobalFile;
+import org.example.sqi_images.drive.domain.DriveEmployee;
 import org.example.sqi_images.part.domain.Part;
 
 import java.util.List;
@@ -27,19 +26,16 @@ public class Employee extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "part_id")
-    private Part part;
-
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "detail_id")
     private EmployeeDetail detail;
 
-    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<DepartmentFile> departmentFiles;
+    @OneToMany(mappedBy = "employee")
+    private List<DriveEmployee> driveEmployees;
 
-    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<GlobalFile> globalFiles;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "part_id")
+    private Part part;
 
     public Employee(String email, String password, String name) {
         this.email = email;
