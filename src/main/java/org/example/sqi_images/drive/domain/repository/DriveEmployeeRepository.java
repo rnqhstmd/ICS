1d29bdd51e743b039ce348acbd39d5da10b8b4b4
BonSeung <bs.koo@sqisoft.com>
2024-09-29 17:12:51 +0900

    feat: 해당 드라이브 권한 찾는 메서드 추가 - #50

--- src/main/java/org/example/sqi_images/drive/domain/repository/DriveEmployeeRepository.java
+++ src/main/java/org/example/sqi_images/drive/domain/repository/DriveEmployeeRepository.java
@@ -2,12 +2,17 @@ package org.example.sqi_images.drive.domain.repository;
 
 import org.example.sqi_images.drive.domain.DriveEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface DriveEmployeeRepository extends JpaRepository<DriveEmployee, Long> {
 
-    Optional<DriveEmployee> findByDriveIdAndEmployeeId(Long driveId, Long employeeId);
+    Optional<DriveEmployee> findByDriveIdAndEmployee_Id(Long driveId, Long employeeId);
 
-    boolean existsByDriveIdAndEmployeeId(Long driveId, Long employeeId);
+    @Query("SELECT de FROM DriveEmployee de WHERE de.drive.id = :driveId AND de.employee.id IN :employeeIds")
+    List<DriveEmployee> findByDriveIdAndEmployeeIds(@Param("driveId") Long driveId, @Param("employeeIds") Set<Long> employeeIds);
 }
