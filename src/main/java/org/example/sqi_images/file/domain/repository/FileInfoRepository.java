643583fefccf91f3a91426173d85f853dcf444c2
BonSeung <bs.koo@sqisoft.com>
2024-09-30 13:35:41 +0900

    feat: 일정 시간이 지난 휴지통 파일 리스트 반환 메서드 추가 - #55

--- src/main/java/org/example/sqi_images/file/domain/repository/FileInfoRepository.java
+++ src/main/java/org/example/sqi_images/file/domain/repository/FileInfoRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
@@ -24,4 +26,8 @@ public interface FileInfoRepository extends JpaRepository<FileInfo, Long> {
             "JOIN FETCH f.employee e " +
             "WHERE f.drive.id = :driveId AND f.isDeleted = true")
     Page<FileInfo> findByDriveIdAndIsDeletedTrue(@Param("driveId") Long driveId, Pageable pageable);
+
+    @Query("SELECT f FROM FileInfo f " +
+            "WHERE f.isDeleted = true AND f.modifiedAt <= :thresholdDate")
+    List<FileInfo> findFilesDeletedOlderThan(@Param("thresholdDate") LocalDateTime thresholdDate);
 }
