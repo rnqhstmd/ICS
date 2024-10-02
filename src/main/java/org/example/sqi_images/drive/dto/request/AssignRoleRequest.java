57c86c305c7cc53af962ba785c01d88f7f8bc934
BonSeung <bs.koo@sqisoft.com>
2024-10-02 10:03:05 +0900

    fix: 잘못된 Validation 어노테이션 제거 - #63

--- src/main/java/org/example/sqi_images/drive/dto/request/AssignRoleRequest.java
+++ src/main/java/org/example/sqi_images/drive/dto/request/AssignRoleRequest.java
@@ -1,12 +1,10 @@
 package org.example.sqi_images.drive.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+
 import org.example.sqi_images.drive.domain.DriveAccessType;
 
 public record AssignRoleRequest(
-        @NotBlank(message = "권한을 수정할 직원의 ID를 입력해주세요.")
         Long employeeId,
-        @NotBlank(message = "수정할 권한을 입력해주세요.")
         DriveAccessType role
 ) {
 }
