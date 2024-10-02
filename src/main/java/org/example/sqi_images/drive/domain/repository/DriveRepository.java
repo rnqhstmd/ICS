0f176b245bc3f4235df229259f02acf0a4b11a8a
BonSeung <bs.koo@sqisoft.com>
2024-10-02 17:41:30 +0900

    feat: 드라이브 정보 조회 쿼리 추가 - #70

--- src/main/java/org/example/sqi_images/drive/domain/repository/DriveRepository.java
+++ src/main/java/org/example/sqi_images/drive/domain/repository/DriveRepository.java
@@ -1,8 +1,17 @@
 package org.example.sqi_images.drive.domain.repository;
 
 import org.example.sqi_images.drive.domain.Drive;
+import org.example.sqi_images.drive.dto.response.DriveInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DriveRepository extends JpaRepository<Drive, Long> {
     boolean existsById(Long driveId);
+
+    @Query("SELECT new org.example.sqi_images.drive.dto.response.DriveInfo(d.id, d.driveName, COUNT(de)) " +
+            "FROM Drive d LEFT JOIN d.driveEmployees de " +
+            "GROUP BY d.id, d.driveName")
+    List<DriveInfo> findAllDrivesWithEmployeeCount();
 }
