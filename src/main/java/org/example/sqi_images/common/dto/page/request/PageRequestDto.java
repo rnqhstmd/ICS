a20629bb0030dd4dd627fcd06f26b962e3603cfe
BonSeung <bs.koo@sqisoft.com>
2024-10-04 09:39:30 +0900

    feat: 이름순 Pageable 메서드 추 - #70

--- src/main/java/org/example/sqi_images/common/dto/page/request/PageRequestDto.java
+++ src/main/java/org/example/sqi_images/common/dto/page/request/PageRequestDto.java
@@ -8,15 +8,21 @@ import org.springframework.data.domain.Sort;
 public record PageRequestDto(int page) {
 
     private static final int PAGE_SIZE = 10;
-    private static final String SORT_BY = "createdAt";
-    private static final Sort.Direction SORT_DIRECTION = Sort.Direction.DESC;
+    private static final String SORT_BY_CREATED_AT = "createdAt";
+    private static final String SORT_BY_NAME = "name";
+    private static final Sort.Direction SORT_DESC = Sort.Direction.DESC;
+    private static final Sort.Direction SORT_ASC = Sort.Direction.ASC;
 
 
     public PageRequestDto(int page) {
         this.page = Math.max(page - 1, 0);
     }
 
-    public Pageable toPageable() {
-        return PageRequest.of(page, PAGE_SIZE, Sort.by(SORT_DIRECTION, SORT_BY));
+    public Pageable toCurrentPageable() {
+        return PageRequest.of(page, PAGE_SIZE, Sort.by(SORT_DESC, SORT_BY_CREATED_AT));
+    }
+
+    public Pageable toNameAscPageable() {
+        return PageRequest.of(page, PAGE_SIZE, Sort.by(SORT_ASC, SORT_BY_NAME));
     }
 }
