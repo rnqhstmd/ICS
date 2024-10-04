ccbc6f52850a844af73ef18749d2614febd5624b
BonSeung <bs.koo@sqisoft.com>
2024-10-04 13:54:03 +0900

    feat: 스웨거 적용 - #75

--- src/main/java/org/example/sqi_images/auth/controller/AuthController.java
+++ src/main/java/org/example/sqi_images/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package org.example.sqi_images.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import static org.example.sqi_images.auth.utils.CookieUtil.clearCookies;
 import static org.example.sqi_images.auth.utils.CookieUtil.setCookie;
 import static org.example.sqi_images.auth.utils.JwtUtil.encode;
 
+@Tag(name = "Auth", description = "권한 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -22,12 +25,14 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "회원가입", description = "새로운 사원을 생성합니다.")
     @PostMapping("/sign-up")
     public ResponseEntity<Void> register(@RequestBody @Valid RegisterDto registerDto) {
         authService.register(registerDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "로그인", description = "사원을 로그인시킵니다.")
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody @Valid LoginDto loginDto, HttpServletResponse response) {
         TokenDto tokenDto = authService.login(loginDto);
@@ -36,6 +41,7 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "로그아웃", description = "사원을 로그아웃시킵니다.")
     @GetMapping("/logout")
     public ResponseEntity<Void> logout(final HttpServletResponse response) {
         clearCookies(response);
--- src/main/java/org/example/sqi_images/photo/controller/PhotoController.java
+++ src/main/java/org/example/sqi_images/photo/controller/PhotoController.java
@@ -1,5 +1,7 @@
 package org.example.sqi_images.photo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.sqi_images.photo.dto.response.PhotoDataResponse;
 import org.example.sqi_images.photo.service.PhotoQueryService;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+@Tag(name = "Photo", description = "이미지 불러오기 API")
 @RestController
 @RequestMapping("/api/images")
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ public class PhotoController {
 
     private final PhotoQueryService photoQueryService;
 
+    @Operation(summary = "특정 이미지 조회", description = "특정 이미지를 조회합니다.")
     @GetMapping("/{photoId}")
     public ResponseEntity<StreamingResponseBody> getImage(@PathVariable Long photoId) {
         PhotoDataResponse photoDataResponse = photoQueryService.getImage(photoId);
